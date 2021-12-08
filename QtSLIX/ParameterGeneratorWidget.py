9aac836fcd618be02ff3d2c71e3badc7d56a1399
Jan André Reuter <jan.andre.reuter@hotmail.de>
2021-12-08 16:54:47 +0100

    Added filtering algorithm from SLIX. Added gpu flag which was missing in one place. Changed locale to US to prevent issues with QDoubleSpinBox. Added a few strings which help the user to separate all parameters.

--- QtSLIX/ParameterGeneratorWidget.py
+++ QtSLIX/ParameterGeneratorWidget.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, \
-    QFileDialog, QCheckBox, QPushButton, QProgressDialog, QSizePolicy
-from PyQt5.QtCore import QCoreApplication, QObject, QThread, pyqtSignal
+    QFileDialog, QCheckBox, QPushButton, QProgressDialog, QSizePolicy, QComboBox, QDoubleSpinBox, QLabel
+from PyQt5.QtCore import QCoreApplication, QObject, QThread, pyqtSignal, QLocale
 
 from .ImageWidget import ImageWidget, convert_numpy_to_qimage
 
@@ -17,7 +17,8 @@ class ParameterGeneratorWorker(QObject):
     finishedWork = pyqtSignal()
     currentStep = pyqtSignal(str)
 
-    def __init__(self, filename, image, output_folder, use_gpu, detailed, min, max, avg, direction, nc_direction, peaks,
+    def __init__(self, filename, image, output_folder, filtering, filtering_parm_1, filtering_parm_2,
+                 use_gpu, detailed, min, max, avg, direction, nc_direction, peaks,
                  peak_width, peak_distance, peak_prominence):
         super().__init__()
         self.filename = filename
@@ -34,6 +35,9 @@ class ParameterGeneratorWorker(QObject):
         self.peak_width = peak_width
         self.peak_distance = peak_distance
         self.peak_prominence = peak_prominence
+        self.filtering = filtering
+        self.filtering_parameter_1 = filtering_parm_1
+        self.filtering_parameter_2 = filtering_parm_2
 
     def process(self):
         output_data_type = ".tiff"
@@ -50,6 +54,23 @@ class ParameterGeneratorWorker(QObject):
         gpu = self.gpu
         detailed = self.detailed
         detailed_str = "_detailed" if detailed else ""
+
+        if QThread.currentThread().isInterruptionRequested():
+            self.finishedWork.emit()
+            return
+
+        if self.filtering != "None":
+            self.currentStep.emit(f"Filtering: {self.filtering} "
+                                  f"{self.filtering_parameter_1} "
+                                  f"{self.filtering_parameter_2}")
+            if self.filtering == "Fourier":
+                self.image = SLIX.preparation.low_pass_fourier_smoothing(self.image,
+                                                                         self.filtering_parameter_1,
+                                                                         self.filtering_parameter_2)
+            elif self.filtering == "Savitzky-Golay":
+                self.image = SLIX.preparation.savitzky_golay_smoothing(self.image,
+                                                                       self.filtering_parameter_1,
+                                                                       self.filtering_parameter_2)
 
         if QThread.currentThread().isInterruptionRequested():
             self.finishedWork.emit()
@@ -160,7 +181,7 @@ class ParameterGeneratorWorker(QObject):
             if detailed:
                 peak_width = SLIX.toolbox.peak_width(self.image, peaks, use_gpu=gpu, return_numpy=True)
             else:
-                peak_width = SLIX.toolbox.mean_peak_width(self.image, peaks)
+                peak_width = SLIX.toolbox.mean_peak_width(self.image, peaks, use_gpu=gpu)
             SLIX.io.imwrite(f'{output_path_name}_peakwidth{detailed_str}'
                             f'{output_data_type}', peak_width)
             del peak_width
@@ -185,10 +206,16 @@ class ParameterGeneratorWidget(QWidget):
     def __init__(self):
         super().__init__()
 
+        self.setLocale(QLocale(QLocale.English, QLocale.UnitedStates))
+
         self.layout = None
         self.sidebar = None
         self.sidebar_button_open_measurement = None
         self.sidebar_button_open_folder = None
+        self.sidebar_checkbox_filtering = None
+        self.sidebar_filtering_algorithm = None
+        self.sidebar_filtering_parameter_1 = None
+        self.sidebar_filtering_parameter_2 = None
         self.sidebar_checkbox_average = None
         self.sidebar_checkbox_minimum = None
         self.sidebar_checkbox_maximum = None
@@ -232,6 +259,38 @@ class ParameterGeneratorWidget(QWidget):
 
         self.sidebar.addStretch(5)
 
+        # Set filtering algorithm
+        self.sidebar_checkbox_filtering = QCheckBox("Filtering")
+        self.sidebar_checkbox_filtering.setChecked(False)
+        self.sidebar.addWidget(self.sidebar_checkbox_filtering)
+        self.sidebar_filtering_algorithm = QComboBox()
+        self.sidebar_filtering_algorithm.setEnabled(False)
+        self.sidebar_filtering_algorithm.addItems(["Fourier", "Savitzky-Golay"])
+        self.sidebar_filtering_algorithm.setCurrentIndex(0)
+        self.sidebar.addWidget(self.sidebar_filtering_algorithm)
+        self.sidebar_checkbox_filtering.stateChanged.connect(self.sidebar_filtering_algorithm.setEnabled)
+        # Set filtering window size
+        self.sidebar_filtering_parameter_1 = QDoubleSpinBox()
+        self.sidebar_filtering_parameter_1.setEnabled(False)
+        self.sidebar_filtering_parameter_1.setRange(0, 1)
+        self.sidebar_filtering_parameter_1.setSingleStep(0.001)
+        self.sidebar_filtering_parameter_1.setValue(0)
+        self.sidebar_filtering_parameter_1.setDecimals(3)
+        self.sidebar.addWidget(self.sidebar_filtering_parameter_1)
+        self.sidebar_checkbox_filtering.stateChanged.connect(self.sidebar_filtering_parameter_1.setEnabled)
+        # Set filtering order / magnitude
+        self.sidebar_filtering_parameter_2 = QDoubleSpinBox()
+        self.sidebar_filtering_parameter_2.setEnabled(False)
+        self.sidebar_filtering_parameter_2.setRange(0, 1)
+        self.sidebar_filtering_parameter_2.setSingleStep(0.001)
+        self.sidebar_filtering_parameter_2.setValue(0)
+        self.sidebar_filtering_parameter_2.setDecimals(3)
+        self.sidebar.addWidget(self.sidebar_filtering_parameter_2)
+        self.sidebar_checkbox_filtering.stateChanged.connect(self.sidebar_filtering_parameter_2.setEnabled)
+
+        self.sidebar.addStretch(1)
+        self.sidebar.addWidget(QLabel("<b>Parameter Maps:</b>"))
+
         self.sidebar_checkbox_average = QCheckBox("Average")
         self.sidebar_checkbox_average.setChecked(False)
         self.sidebar.addWidget(self.sidebar_checkbox_average)
@@ -269,6 +328,8 @@ class ParameterGeneratorWidget(QWidget):
         self.sidebar.addWidget(self.sidebar_checkbox_peaks)
 
         self.sidebar.addStretch(1)
+
+        self.sidebar.addWidget(QLabel("<b>Other options:</b>"))
 
         self.sidebar_checkbox_detailed = QCheckBox("Detailed")
         self.sidebar_checkbox_detailed.setChecked(False)
@@ -323,8 +384,16 @@ class ParameterGeneratorWidget(QWidget):
 
         dialog = QProgressDialog("Generating...", "Cancel", 0, 0, self)
 
+        if self.sidebar_checkbox_filtering.isChecked():
+            filtering_algorithm = self.sidebar_filtering_algorithm.currentText()
+        else:
+            filtering_algorithm = "None"
+
         worker_thread = QThread()
         worker = ParameterGeneratorWorker(self.filename, self.image, output_folder,
+                                          filtering_algorithm,
+                                          self.sidebar_filtering_parameter_1.value(),
+                                          self.sidebar_filtering_parameter_2.value(),
                                           self.sidebar_checkbox_use_gpu.isChecked(),
                                           self.sidebar_checkbox_detailed.isChecked(),
                                           self.sidebar_checkbox_minimum.isChecked(),
