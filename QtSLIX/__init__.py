7191134bf45e6c5bc99b409af1bca51018ef2d29
Jan André Reuter <jan.andre.reuter@hotmail.de>
2021-12-10 15:48:32 +0100

    Allow install thorugh pip install . when SLIX 2.4.0 gets released

--- QtSLIX/__init__.py
+++ QtSLIX/__init__.py
@@ -2,7 +2,7 @@
 Qt Scattered Light Imaging ToolboX (QtSLIX) – an open source graphical user interface for the Scattered Light
 Imaging ToolboX (SLIX).
 """
-__version__ = '0.0.1'
-__all__ = ['MainWindow']
+__version__ = '1.0.0'
+__all__ = ['MainWindow', 'ClusterWidget', 'ImageWidget', 'VisualizationWidget', 'ParameterGeneratorWidget']
 
-from . import MainWindow
+from . import MainWindow, ClusterWidget, ImageWidget, VisualizationWidget, ParameterGeneratorWidget
